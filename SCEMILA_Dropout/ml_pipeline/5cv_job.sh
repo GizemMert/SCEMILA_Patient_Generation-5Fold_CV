208a7579998b36ae175669f131ab8d855c25ab19
GizemMert <gizem.mert12@gmail.com>
2024-06-02 13:54:50 +0200

    job script is added.

---
+++ SCEMILA_Dropout/ml_pipeline/5cv_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=SC_5F_CV
+#SBATCH --partition=gpu_p
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=50G
+#SBATCH --gres=gpu:1
+#SBATCH --time=5:00:00
+#SBATCH --nice=1
+#SBATCH --qos=gpu_short
+#SBATCH --output=5F_CV_output.log
+#SBATCH --error=.5F_CV_error.log
+
+source /home/aih/gizem.mert/miniconda3/etc/profile.d/conda.sh
+conda activate my_env2
+
+python run_pipeline.py --result_folder=TEST_RUN
