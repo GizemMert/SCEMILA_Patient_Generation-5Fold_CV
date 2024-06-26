68d5e6a538036f6038215b376734c808cc2f71bb
GizemMert <gizem.mert12@gmail.com>
2024-06-26 12:25:01 +0200

    rerun

--- SCEMILA/create_artifcial_patients/create_combined_dataset_naive.py
+++ SCEMILA/create_artifcial_patients/create_combined_dataset_naive.py
@@ -51,7 +51,7 @@ def copyfiles_art(src_folder, dest_folder, patient_counts):
     return selected_patients, paths
 
 # Specify your source and destination folders
-src_data_folder = '/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/Data/data'
+src_data_folder = '/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/Data/data/data'
 src_artificialdata_folder = f'/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/Data/artificialdata/experiment_3/data'
 mixeddata_folder = f'/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/Data/mixed_seed{seed}'
 percentage = [10, 20, 30, 50]
