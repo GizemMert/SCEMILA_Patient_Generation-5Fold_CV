8c91c4123f29883f273c851c92b68ed2c2b8ddee
GizemMert <gizem.mert12@gmail.com>
2024-06-30 21:57:09 +0200

    rerun

--- SCEMILA_Dropout/ml_pipeline/run_pipeline.py
+++ SCEMILA_Dropout/ml_pipeline/run_pipeline.py
@@ -26,7 +26,7 @@ torch.multiprocessing.set_sharing_strategy('file_system')
 #SOURCE_FOLDER = r'/mnt/c/Users/Hillary Hauger/Documents/Studium/WS23-24/Computational Methods for Single-cell Biology/smalldataset'
 #Random shuffe: Experiment 1
 TARGET_FOLDER = '/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/result_folder_2'
-SOURCE_FOLDER = '/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/Data/data/data'
+SOURCE_FOLDER = '/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/Data/data'
 # get arguments from parser, set up folder
 # parse arguments
 parser = ap.ArgumentParser()
