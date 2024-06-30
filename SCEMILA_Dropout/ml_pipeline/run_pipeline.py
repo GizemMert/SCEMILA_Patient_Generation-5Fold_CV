e22a9d73b093e90dd72e23777b2e66330af32c6b
GizemMert <gizem.mert12@gmail.com>
2024-06-30 22:04:18 +0200

    rerun

--- SCEMILA_Dropout/ml_pipeline/run_pipeline.py
+++ SCEMILA_Dropout/ml_pipeline/run_pipeline.py
@@ -25,7 +25,7 @@ torch.multiprocessing.set_sharing_strategy('file_system')
 # path to dataset
 #SOURCE_FOLDER = r'/mnt/c/Users/Hillary Hauger/Documents/Studium/WS23-24/Computational Methods for Single-cell Biology/smalldataset'
 #Random shuffe: Experiment 1
-TARGET_FOLDER = '/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/result_folder_2'
+TARGET_FOLDER = '/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/Data/result_folder_2'
 SOURCE_FOLDER = '/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/Data/data'
 TRAIN_VAL_FOLDER = os.path.join(TARGET_FOLDER, "train_val_data")
 TEST_FOLDER = os.path.join(TARGET_FOLDER, "test_data")
