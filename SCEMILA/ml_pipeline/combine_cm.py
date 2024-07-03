46ffd63130d1952946a581bb123450b352c1b460
GizemMert <gizem.mert12@gmail.com>
2024-07-03 15:13:29 +0200

    rerun

---
+++ SCEMILA/ml_pipeline/combine_cm.py
@@ -0,0 +1,52 @@
+import numpy as np
+import os
+import label_converter
+
+
+
+base_path = "/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV"
+fold_paths = [
+    os.path.join(base_path, f"result_fold_{i}_mixed/mixed_seed42_max20/test_conf_matrix.npy")
+    for i in range(5)
+]
+
+# Load and sum the confusion matrices from all folds
+sum_confusion_matrix = None
+for fold_path in fold_paths:
+    if os.path.exists(fold_path):
+        confusion_matrix = np.load(fold_path)
+        if sum_confusion_matrix is None:
+            sum_confusion_matrix = confusion_matrix
+        else:
+            sum_confusion_matrix += confusion_matrix
+
+# Define the reorder list for confusion matrix display
+reorder = ['PML_RARA', 'NPM1', 'CBFB_MYH11', 'RUNX1_RUNX1T1', 'control']
+
+# Define the function to save the confusion matrix
+def save_confusion_matrix(confusion_data, lbl_conv_obj, fig_export_path):
+    # Save confusion matrix as npy file
+    np.save(os.path.join(fig_export_path, 'confusion_matrix.npy'), confusion_data)
+
+    # Plot and save confusion matrix as SVG
+    confusion_matrix.show_pred_mtrx(
+        pred_mtrx=confusion_data,
+        class_conversion=lbl_conv_obj.df,
+        reorder=reorder,
+        fig_size=(8.1, 4.5),
+        path_save=os.path.join(fig_export_path, 'confusion_matrix.svg')
+    )
+
+# Define the path to the label conversion file
+label_conv_obj = label_converter.LabelConverter(
+    path_preload=os.path.join(base_path, "result_fold_0/class_conversion.csv")
+)
+
+# Define the path to save the final confusion matrix
+fig_export_path = "/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/combined_cm"
+
+# Create the directory if it does not exist
+os.makedirs(fig_export_path, exist_ok=True)
+
+# Save the summed confusion matrix
+save_confusion_matrix(sum_confusion_matrix, label_conv_obj, fig_export_path)
