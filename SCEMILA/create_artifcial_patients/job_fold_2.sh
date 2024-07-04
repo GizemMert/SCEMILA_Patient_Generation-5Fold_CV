1ab5e39381eed085f68fef811ec4e8d45a8c944d
GizemMert <gizem.mert12@gmail.com>
2024-07-04 22:08:08 +0200

    rerun

--- SCEMILA/create_artifcial_patients/job_fold_2.sh
+++ SCEMILA/create_artifcial_patients/job_fold_2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=fol_4
+#SBATCH --job-name=fol_2
 #SBATCH --partition=gpu_p
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -10,10 +10,10 @@
 #SBATCH --nice=10000
 #SBATCH --qos=gpu_normal
 
-#SBATCH --output=FOLD_4_output_3.log
-#SBATCH --error=FOLD_4_error_3.log
+#SBATCH --output=FOLD_2_output_3.log
+#SBATCH --error=FOLD_2_error_3.log
 
 source /home/aih/gizem.mert/miniconda3/etc/profile.d/conda.sh
 conda activate my_new_env
 
-python create_combined_dataset_uncertainty_fold_4.py
+python create_combined_dataset_uncertainty_fold_2.py
