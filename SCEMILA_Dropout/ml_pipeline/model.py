c273970991b225fb9fe4c8479f9410efe6136275
GizemMert <gizem.mert12@gmail.com>
2024-07-10 19:54:53 +0200

    rerun

--- SCEMILA_Dropout/ml_pipeline/model.py
+++ SCEMILA_Dropout/ml_pipeline/model.py
@@ -67,7 +67,7 @@ class AMiL(nn.Module):
             self.classifier_multi_column.append(nn.Sequential(
                 nn.Linear(self.L, 64),
                 nn.ReLU(),
-                nn.Dropout(p = 0.2),
+                nn.Dropout(p = 0.4),
                 nn.Linear(64, 1)
             ))
 
