90a98b45a86ee9d8073fcf4ec0bf8d45a14169b2
GizemMert <gizem.mert12@gmail.com>
2024-07-10 16:29:08 +0200

    rerun

--- SCEMILA/create_artifcial_patients/job_fold_4.sh
+++ SCEMILA/create_artifcial_patients/job_fold_4.sh
@@ -16,4 +16,4 @@
 source /home/aih/gizem.mert/miniconda3/etc/profile.d/conda.sh
 conda activate my_new_env
 
-python create_combined_dataset_uncertainty_fold_4.py
+python 3_shuffle_images_distribution_fold4.py
