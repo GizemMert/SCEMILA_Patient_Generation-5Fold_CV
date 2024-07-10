a370ab0bf3ac581533d357630580b266642505f8
GizemMert <gizem.mert12@gmail.com>
2024-07-10 17:00:11 +0200

    rerun

--- SCEMILA/ml_pipeline/job_fold_3.sh
+++ SCEMILA/ml_pipeline/job_fold_3.sh
@@ -16,4 +16,4 @@
 source /home/aih/gizem.mert/miniconda3/etc/profile.d/conda.sh
 conda activate my_new_env
 
-python run_pipeline_mixed_fold_3.py --result_folder=/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/result_fold_3_mixed/mixed_seed42_max20/result_folder --source_folder=/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/Data/mixed_uncertain_fold_3_seed42/max_20_percent --target_folder=/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/result_fold_3_mixed/mixed_seed42_max20
+python run_pipeline_mixed_fold_3.py --result_folder=/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/result_fold_3_mixed/mixed_seed42_max10/result_folder --source_folder=/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/Data/mixed_uncertain_fold_3_seed42/max_10_percent --target_folder=/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/result_fold_3_mixed/mixed_seed42_max10
