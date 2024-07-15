0c1a2746e31f33238545ada9ed7d4576cca5ce0a
GizemMert <gizem.mert12@gmail.com>
2024-07-15 11:44:32 +0200

    rerun

--- SCEMILA_Dropout/ml_pipeline/run_pipeline.py
+++ SCEMILA_Dropout/ml_pipeline/run_pipeline.py
@@ -47,7 +47,7 @@ parser.add_argument(
     '--ep',
     help='max. amount after which training should stop',
     required=False,
-    default=150)             # epochs to train
+    default=70)             # epochs to train
 parser.add_argument(
     '--es',
     help='early stopping if no decrease in loss for x epochs',
