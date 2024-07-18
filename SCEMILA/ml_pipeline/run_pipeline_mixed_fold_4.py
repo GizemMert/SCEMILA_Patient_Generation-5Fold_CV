a54e4cdc63113ae951a52cbd2e2503a98f0e2524
GizemMert <gizem.mert12@gmail.com>
2024-07-18 15:22:09 +0200

    rerun

--- SCEMILA/ml_pipeline/run_pipeline_mixed_fold_4.py
+++ SCEMILA/ml_pipeline/run_pipeline_mixed_fold_4.py
@@ -151,8 +151,8 @@ print("")
 print('Initialize datasets...')
 with open(args.source_folder+'/file_paths.pkl', 'rb') as f:
     mixed_data_filepaths = pickle.load(f)
-label_conv_obj = label_converter.LabelConverter(path_preload="/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/result_fold_0/class_conversion.csv")
-# label_conv_obj = label_converter.LabelConverter()
+# label_conv_obj = label_converter.LabelConverter(path_preload="/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/result_fold_0/class_conversion.csv")
+label_conv_obj = label_converter.LabelConverter()
 set_dataset_path(args.source_folder)
 define_dataset(
     num_folds=4,
@@ -181,7 +181,7 @@ datasets['val'] = MllDataset(
     folds=folds['val'],
     aug_im_order=False,
     split='val')
-label_conv_obj = label_converter.LabelConverter(path_preload="/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/result_fold_0/class_conversion.csv")
+# label_conv_obj = label_converter.LabelConverter(path_preload="/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/result_fold_0/class_conversion.csv")
 set_dataset_path("/home/aih/gizem.mert/SCEMILA_5K/SCEMILA_Patient_Generation-5Fold_CV/Data/Folds/fold_4/test")
 define_dataset(
     num_folds=1,
